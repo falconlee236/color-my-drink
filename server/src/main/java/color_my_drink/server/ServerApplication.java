1124207f07d79c6bb5bb86406af0cd3de66d35e4
falconlee236 <falconlee236@cau.ac.kr>
2025-01-04 16:34:07 +0900

    init project

---
+++ server/src/main/java/color_my_drink/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package color_my_drink.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/color_my_drink/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package color_my_drink.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
