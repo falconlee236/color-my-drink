d13190da9d4273c45dfdd0932230e91ceb2ba0ee
elfffffy <wjd1278@naver.com>
2025-02-13 18:00:46 +0900

    Steps

---
+++ web/src/component/PrevBtn.tsx
@@ -0,0 +1,7 @@
+function PrevBtn () {
+    return (
+        <button className="bg-ccc80 rounded-20 text-white font-medium text-[16px] w-full h-55 col-span-1">이전</button>
+    )
+}
+
+export default PrevBtn
