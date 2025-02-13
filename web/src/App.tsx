3217b44d0923ac5295218d14f0adbec226895719
elfffffy <wjd1278@naver.com>
2025-02-13 17:37:25 +0900

    branch 정리..

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Login from "./Login.tsx";
-import Mainpage from "./Mainpage.tsx";
-import MainpageF from "./MainpageF.tsx";
-import Step1 from "./Step1.tsx";
-import Step2 from "./Step2.tsx";
-import Step3 from "./Step3.tsx";
-import Result from "./Result.tsx";
-import Loading from "./Loading.tsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/mainpage" element={<Mainpage />} />
-        <Route path="/mainpageF" element={<MainpageF />} />
-        <Route path="/step1" element={<Step1 />} />
-        <Route path="/step2" element={<Step2 />} />
-        <Route path="/step3" element={<Step3 />} />
-        <Route path="/loading" element={<Loading />} />
-        <Route path="/result" element={<Result />} />
       </Routes>
     </BrowserRouter>
   );
--- web/src/component/PrevBtn.tsx
+++
@@ -1,7 +0,0 @@
-function PrevBtn () {
-    return (
-        <button className="bg-ccc80 rounded-20 text-white font-medium text-[16px] w-full h-55 col-span-1">이전</button>
-    )
-}
-
-export default PrevBtn
